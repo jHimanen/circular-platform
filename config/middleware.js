b90c10619b5292b6d4a5ab0e96d4e16abb17a094
Joel Himanen <joel.himanen@aalto.fi>
2021-07-28 11:57:50 +0300

    Set up frontend skeleton and link with CMS

---
+++ config/middleware.js
@@ -0,0 +1,9 @@
+/*module.exports = ({ env }) => ({
+    settings: {
+        cors: {
+            enabled: true,
+            headers: '*',
+            origin: ['http://localhost:3000/', 'http://localhost:1337/'],
+        },
+    },
+});*/
