700584a5e02043c63cfafbe6b9f799236b9e74eb
Joel Himanen <joel.himanen@aalto.fi>
2021-08-05 14:13:37 +0300

    Get rid of redundant logging

--- config/functions/news.js
+++ config/functions/news.js
@@ -8,19 +8,16 @@ const url = 'https://newsapi.org/v2/everything?' +
             `apiKey=${process.env.NEWS_API_KEY}`
 
 module.exports = async () => {
-    console.log('Making the API call...')
     const { data } = await axios.get(url)
     const articles = data.articles.length < 5
                     ? data.articles
                     : data.articles.slice(0, 5)
 
     articles.forEach(article => {
-        console.log(article.title)
         strapi.query('article').create({
             Title: article.title,
             Description: article.description,
             URL: article.url
         })
     })
-    console.log('Exiting the API caller...')
 }
--- config/middleware.js
+++
@@ -1,9 +0,0 @@
-/*module.exports = ({ env }) => ({
-    settings: {
-        cors: {
-            enabled: true,
-            headers: '*',
-            origin: ['http://localhost:3000/', 'http://localhost:1337/'],
-        },
-    },
-});*/
