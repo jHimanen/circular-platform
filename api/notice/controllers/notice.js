e364c1d03d77c6bab1620b634d5694f48f600272
Joel Himanen <joel.himanen@aalto.fi>
2021-08-27 17:26:53 +0300

    Configure 'notices' controller to filter content by user tags

--- api/notice/controllers/notice.js
+++ api/notice/controllers/notice.js
@@ -1,8 +1,35 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
 
-module.exports = {};
+    async find(ctx) {
+
+        let entities;
+        const splitURL = ctx.request.url.split('=')
+
+        if (splitURL.length === 2) {
+
+            const token = await strapi.query('token').findOne({ token: splitURL[1]})
+            const userTags = await strapi.query('tag').find({ id_in: token.user.tags })
+            const allNotices = await strapi.services.notice.find()
+
+            entities = allNotices.filter(notice => 
+                notice.tags.map(tag => tag.id).some(noticeTagID => 
+                    userTags.map(tag => tag.id).indexOf(noticeTagID) >= 0
+                )
+            )
+            
+        } else {
+
+            if (ctx.query._q) {
+                entities = await strapi.services.notice.search(ctx.query);
+            } else {
+                entities = await strapi.services.notice.find(ctx.query);
+            }
+
+        }
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.notice }));
+    }
+
+};
+
